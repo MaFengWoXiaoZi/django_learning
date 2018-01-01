ba3ceabfddd3b3b9f950f9403440aff3aa80064a
haoran <mafengwoxiaozi@gmail.com>
2018-01-02 00:37:36 +0800

    views.py usage1 exercise Django.http.HttpRequest

--- hello_django/hello/views.py
+++ hello_django/hello/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
 def hello(request, a):
    print(a)
+   print(isinstance(request, HttpRequest)) # True
+   print(request.path) # return request path
+   print(request.get_full_path()) # return entire path
+   print(request.method) # GET
+   print(request.GET.get('name'))
+   print(request.GET.get('key')) # GET method get value
+   print(request.POST.get('key')) # POST method get value
    user_list = User.objects.all()
    return render(request, 'table.html', {'user_list': user_list})
 
